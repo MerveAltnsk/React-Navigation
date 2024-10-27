a435eeb3b3f1f1c9d0abefa923ad9f87bac0e1d7
merve <mervealtinisik99@gmail.com>
2024-10-27 17:15:06 +0300

    first commit

---
+++ src/Pages/Second.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+function Second() {
+    return (
+        <View>
+            <Text>Hello Second</Text>
+        </View>
+    );
+}
+export default Second;
---
+++ src/Router.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';       
+import {createStackNavigator} from '@react-navigation/stack';
+
+import First from './Pages/First';
+import Second from './Pages/Second';
+
+
+const Stack = createStackNavigator();
+
+functionApp(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="FirstScreen" component={FirstScreen} />
+        <Stack.Screen name="SecondScreen" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
